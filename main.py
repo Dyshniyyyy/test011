f60fa1ebeea7aca21714dbbbbb9c373515d211e4
Sch1561-it-047 <Sch1561-it-047@sch1561.mskobr.ru>
2023-01-12 17:28:34 +0300

    Пофиксил

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 def sum(a,b):
     return a + b
+
+
+def sub(a,b):
+    return a - b
+
+
 a, sign, b = input().split()
 
 if sign == "+":
     print(sum(int(a), int(b)))
 
-def sub(a,b):
-    return a - b
-a, sign, b = input().split()
-
 if sign == "-":
     print(sub(int(a), int(b)))
+
 
 
 
