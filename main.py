9a85a042944312143eb71dafa2ec6260fc28a2fe
Sch1561-it-047 <Sch1561-it-047@sch1561.mskobr.ru>
2023-01-12 17:23:52 +0300

    Сделал вычетание

--- main.py
+++ main.py
@@ -4,3 +4,72 @@ a, sign, b = input().split()
 
 if sign == "+":
     print(sum(int(a), int(b)))
+
+def sub(a,b):
+    return a - b
+a, sign, b = input().split()
+
+if sign == "-":
+    print(sub(int(a), int(b)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
